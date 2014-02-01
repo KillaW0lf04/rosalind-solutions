5b3e889e4cdfaac9475671da3cdeb38d887fbb41
Michael Aquilina <michaelaquilina@gmail.com>
2014-02-01 18:51:48 +0000

    Fix to minor bug

--- src/problem3.py
+++ src/problem3.py
@@ -9,7 +9,7 @@ Given: A DNA string s of length at most 1000 bp.
 Return: The reverse complement sc of s.
 """
 
-from utils import reverse_complementg
+from utils import reverse_complement
 
 
 if __name__ == '__main__':
