86ea77799969dd3b77e6f74d2e3f4614ab5e791a
Michael Aquilina <michaelaquilina@gmail.com>
2014-02-02 12:24:59 +0000

    Add Genetic Code to utils

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,27 @@
 from collections import Counter
 
 
+GENETIC_CODE = {
+    'UUU': 'F',      'CUU': 'L',      'AUU': 'I',      'GUU': 'V',
+    'UUC': 'F',      'CUC': 'L',      'AUC': 'I',      'GUC': 'V',
+    'UUA': 'L',      'CUA': 'L',      'AUA': 'I',      'GUA': 'V',
+    'UUG': 'L',      'CUG': 'L',      'AUG': 'M',      'GUG': 'V',
+    'UCU': 'S',      'CCU': 'P',      'ACU': 'T',      'GCU': 'A',
+    'UCC': 'S',      'CCC': 'P',      'ACC': 'T',      'GCC': 'A',
+    'UCA': 'S',      'CCA': 'P',      'ACA': 'T',      'GCA': 'A',
+    'UCG': 'S',      'CCG': 'P',      'ACG': 'T',      'GCG': 'A',
+    'UAU': 'Y',      'CAU': 'H',      'AAU': 'N',      'GAU': 'D',
+    'UAC': 'Y',      'CAC': 'H',      'AAC': 'N',      'GAC': 'D',
+    'UAA': None,     'CAA': 'Q',      'AAA': 'K',      'GAA': 'E',
+    'UAG': None,     'CAG': 'Q',      'AAG': 'K',      'GAG': 'E',
+    'UGU': 'C',      'CGU': 'R',      'AGU': 'S',      'GGU': 'G',
+    'UGC': 'C',      'CGC': 'R',      'AGC': 'S',      'GGC': 'G',
+    'UGA': None,     'CGA': 'R',      'AGA': 'R',      'GGA': 'G',
+    'UGG': 'W',      'CGG': 'R',      'AGG': 'R',      'GGG': 'G',
+}
+
+
+
 def distance(s1, s2):
     return sum([1 for c1, c2 in zip(s1, s2) if c1 != c2])
 
