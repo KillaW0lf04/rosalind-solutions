63696af5c87a4343af7dfc2051ca24f65e088cf8
Michael Aquilina <michaelaquilina@gmail.com>
2014-02-03 08:32:58 +0000

    Add solution for finding a motif in DNA

---
+++ src/problem9.py
@@ -0,0 +1,21 @@
+
+def occurrences(s, t):
+    if len(t) > len(s):
+        return None
+
+    results = []
+
+    for i, _ in enumerate(s):
+        if s[i:i + len(t)] == t:
+            results.append(i + 1)
+
+    return results
+
+
+if __name__ == '__main__':
+
+    with open('data/rosalind_subs.txt') as f:
+        s = f.readline().rstrip()
+        t = f.readline().rstrip()
+
+    print ' '.join("%s" % s for s in occurrences(s, t))
